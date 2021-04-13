98130ae5fdfc6a6d4f39fcc5e444ee4039bec4ab
bopos2 <pakawat.work88@gmail.com>
2021-04-14 04:02:17 +0700

    part5 - Update Data into

--- admin/code.php
+++ admin/code.php
@@ -29,5 +29,25 @@ session_start();
     }
 
 
-    
+    if(isset($_POST['updatebtn']))
+    {
+        $id = $_POST['edit_id'];
+        $username = $_POST['edit_username'];
+        $email = $_POST['edit_email'];
+        $password = $_POST['edit_password'];
+        
+        $query = "UPDATE register SET username='$username',email='$email',password='$password' WHERE id='$id' ";
+        $query_run = mysqli_query($connection,$query);
+
+        if($query_run )
+        {
+            $_SESSION['success'] = "Your Data is Updated";
+            header('Location: register.php');
+        }
+        else
+        {
+            $_SESSION['status'] = "Your Data is NOT Updated";
+            header('Location: register.php');
+        }
+    }
 ?>
