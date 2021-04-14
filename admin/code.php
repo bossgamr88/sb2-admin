4e630f837957ef262d076cd932e48e64329cf836
bopos2 <pakawat.work88@gmail.com>
2021-04-14 07:21:01 +0700

    login system

--- admin/code.php
+++ admin/code.php
@@ -70,7 +70,26 @@ session_start();
        
     }
 
+    if(isset($_POST['login_btn']))
+    {
+        $email_login = $_POST['email'];
+        $password_login = $_POST['password'];
 
+        $query = "SELECT * FROM register WHERE id='$email_login' AND password='$password_login' ";
+        $query_run = mysqli_query($connection,$query);
+
+        if($query_run )
+        {
+            $_SESSION['username'] = $email_login;
+            header('Location: index.php');
+        }
+        else
+        {
+            $_SESSION['status'] = "Email id / Password is Invalid";
+            header('Location: login.php');
+        }
+       
+    }
 
 
 
