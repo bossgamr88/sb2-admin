ea58633cfcfcc13aeaef50c56f266716405096f4
bopos2 <pakawat.work88@gmail.com>
2021-05-07 17:46:26 +0700

    test test

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@ include('includes/header.php');?>
         <div class="col-md-12 mr-auto ml-auto text-center py-5 mt-5 p-5">
             <div class="card">
                 <div class="card-body text-center">
-                    <h1 class="card-title bg-warning">Error Page</h1>
+                    <h1 class="card-title bg-warning">Error Page Test</h1>
                     <h2 class="card-title">404 Error</h2>
                     <p class="card-text">The Page You are searching for is Not Available.</p>
                     <a href="index.php" class="btn btn-primary">Go back to Home Page</a>
